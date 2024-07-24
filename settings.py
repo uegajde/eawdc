c0fa42cbc0ca4a56e524dc4c9aa6d71b8fc01f43
Zhe-Hui Lin <uegajde@gmail.com>
2024-07-24 20:08:45 +0800

    adjust default settings

--- settings.py
+++ settings.py
@@ -6,10 +6,10 @@ destinationDir = './download/'
 
 # doDownload
 # perform download, set to False when you just want to obtain the urls
-doDownload = False
+doDownload = True
 
 # show the url list going to request
-show_toDownload = True
+show_toDownload = False
 
 # blankTime:
 # blank time for the data productor preparing the latest one
@@ -24,11 +24,11 @@ urlgenhelper.timeConfigure('JMA_Weather_Chart_AUPQ78'       , "hour"  , maxHr   
 urlgenhelper.timeConfigure('JMA_Weather_Chart_AUPQ35'       , "hour"  , maxHr   , 12, False)
 urlgenhelper.timeConfigure('CWA_Surface_Analysis'           , "hour"  , maxHr   ,  6, False)
 urlgenhelper.timeConfigure('CWA_Skew'                       , "hour"  , maxHr   , 12, False)
-urlgenhelper.timeConfigure('CWA_Radar'                      , "minute", maxHr*60, 10, False)
-urlgenhelper.timeConfigure('CWA_Satellite_Visible_EastAsia' , "minute", maxHr*60, 10, False)
-urlgenhelper.timeConfigure('CWA_Satellite_Visible_TW'       , "minute", maxHr*60, 10, False)
-urlgenhelper.timeConfigure('CWA_Satellite_Infrared_EastAsia', "minute", maxHr*60, 10, False)
-urlgenhelper.timeConfigure('CWA_Satellite_Infrared_TW'      , "minute", maxHr*60, 10, False)
+urlgenhelper.timeConfigure('CWA_Radar'                      , "minute", maxHr*60, 30, False)
+urlgenhelper.timeConfigure('CWA_Satellite_Visible_EastAsia' , "minute", maxHr*60, 30, False)
+urlgenhelper.timeConfigure('CWA_Satellite_Visible_TW'       , "minute", maxHr*60, 30, False)
+urlgenhelper.timeConfigure('CWA_Satellite_Infrared_EastAsia', "minute", maxHr*60, 30, False)
+urlgenhelper.timeConfigure('CWA_Satellite_Infrared_TW'      , "minute", maxHr*60, 30, False)
 urlgenhelper.timeConfigure('CWA_Surface_Temperature'        , "hour"  , maxHr   ,  1, False)
 urlgenhelper.timeConfigure('CWA_Precipitation'              , "minute", maxHr*60, 30, False)
 urlgenhelper.timeConfigure('CWA_Precipitation_ex'           , "minute", maxHr*60, 30, False)
